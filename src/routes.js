39721efb9323274cca6dce2ba0000bf00b5ccb1b
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-09 21:47:19 +0100

    home and cart components plus route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
 
 export default function Routes() {
   return (
-    <BrowserRouter>
       <Switch>
-
+        <Route path="/" exact componet={Home} />
+        <Route path="/cart" componet={Cart} />
       </Switch>
-    </BrowserRouter>
   );
 }
