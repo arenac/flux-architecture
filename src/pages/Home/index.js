5290a502f7ca95baf0f8ed2c1b927cc20f4c2b52
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-10 00:08:24 +0100

    styling home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,122 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
+          alt="Shoes"
+        />
+        <strong>Nice shoes</strong>
+        <span>1300.00 Kr</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>Add to cart</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
