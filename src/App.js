c1b02e0b768dc141e90f21b719286e23dba380c8
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-09 22:16:58 +0100

    missing package and route fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-// import Routes from './routes';
+import Routes from './routes';
 // import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Hello Redux</h1>
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
