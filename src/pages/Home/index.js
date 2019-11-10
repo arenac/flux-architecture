5e4ba1f563d9f796e9a79e953533329a81409e1a
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-10 01:11:46 +0100

    fetching api data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,122 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
-export default function Home() {
+import api from '../../services/api';
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await api.get('products');
+      setProducts(response.data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
+      {products.map((product, key) => (
+        <li key={String(key)}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.price}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
 
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://c.static-nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/xnvkdpbwtevczwlo1sbj/air-max-plus-older-shoe-7DdWK3.jpg"
-          alt="Shoes"
-        />
-        <strong>Nice shoes</strong>
-        <span>1300.00 Kr</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>Add to cart</span>
-        </button>
-      </li>
+            <span>Add to cart</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
-}
+};
+
+export default Home;
